225133a1bb9ec275aa4a8472e5321e1fd6fe9e9d
IGauto04 <63121360+zain-ali-12@users.noreply.github.com>
2022-03-04 23:25:39 +0400

    new structure

--- manager.py
+++ manager.py
@@ -7,6 +7,10 @@ print(root)
 def generate_suras_files():
     os.chdir(r"C:\Users\Zain Ali\Documents\GitHub\Al-Quran\Suras")
     for i in range(114):
-        with open(str(i+1)+".txt", 'w') as new_file:
+        i = str(i+1)
+        while len(i) != 3:
+            i = "0" + i
+        with open(i+".txt", 'w') as new_file:
             pass
 
+generate_suras_files()
